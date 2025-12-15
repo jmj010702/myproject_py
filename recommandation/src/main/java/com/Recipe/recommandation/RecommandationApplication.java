e480e5b8d1fffbe624c46bddb26135fb4af14604
jmj010702 <bnm07@naver.com>
2025-12-15 12:24:36 +0900

    스트링부트 파일

---
+++ recommandation/src/main/java/com/Recipe/recommandation/RecommandationApplication.java
@@ -0,0 +1,13 @@
+package com.Recipe.recommandation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecommandationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecommandationApplication.class, args);
+	}
+
+}
---
+++ recommandation/src/test/java/com/Recipe/recommandation/RecommandationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Recipe.recommandation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommandationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
